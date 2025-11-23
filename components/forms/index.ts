a129a082cb1cc3097550d96a77b5cbf9d1137f3b
YKY227 <48209108+YKY227@users.noreply.github.com>
2025-11-23 16:34:53 +0800

    Add files via upload

---
+++ components/forms/index.ts
@@ -0,0 +1,2 @@
+// Placeholder barrel file for forms components.
+// Add and export your React components from here.
---
+++ components/ui/index.ts
@@ -0,0 +1,2 @@
+// Placeholder barrel file for ui components.
+// Add and export your React components from here.
