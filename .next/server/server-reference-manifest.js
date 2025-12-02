657babe29bb41fa116ea07e7e5c4527c70ff1a02
YKY227 <48209108+YKY227@users.noreply.github.com>
2025-12-02 08:49:14 +0000

    Organize project into frontend and backend folders

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZXw6+fRKaZihZV9l+wA7acxjQgszL8C+kuCLY1SyTqA=\"\n}"
---
+++ courier-system-frontend/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/5L8eFK8nfvXX9sLhP2QQsvQG40YNAJQ0Mv3TwKE/C0=\"\n}"
